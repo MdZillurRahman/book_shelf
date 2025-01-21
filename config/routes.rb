30334be06e1a0e1afd35bfb8db8d419b5aeda665
MdZillurRahman <mzisanr@gmail.com>
2025-01-21 23:38:01 +0600

    updated homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/", to: "main#index"
+  # get "/", to: "main#index"
+  root to: "main#index"
+
+  get "explore", to: "explore#index", as: :explore
 end
