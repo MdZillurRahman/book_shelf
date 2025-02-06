d9755326e42baee5b0f0454abcc39a0bf12a6f44
MdZillurRahman <mzisanr@gmail.com>
2025-02-06 23:34:56 +0600

    clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get "/", to: "main#index"
